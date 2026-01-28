08573ebcbf78b2a511becad0bd3241d251e127f2
riyandabella <riyandabella561@gmail.com>
2026-01-28 13:51:25 +0800

    Add project files.

---
+++ Controllers/BukuController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using tesKPL.Models;
+
+namespace tesKPL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BukuController : ControllerBase
+    {
+
+        public string filePath = "Data/books.json";
+
+        public List<Buku> ReadJson()
+        {
+           if (!System.IO.File.Exists(filePath))
+            {
+                return new List<Buku>();
+            }
+
+            var json = System.IO.File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<Buku>>(json) ?? new List<Buku>();
+        }
+
+        public void WriteJson(List<Buku> buku)
+        {
+            var json = JsonSerializer.Serialize(buku, new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            });
+        }
+
+        [HttpGet]
+        public ActionResult<List<Buku>> GetBuku()
+        {
+            return ReadJson();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<List<Buku>> GetBukuById(int id, Buku buku)
+        {
+            if (id < 0)
+            {
+                return NotFound("pastikan id benar");
+            }
+
+            return ReadJson(buku[id]);
+        }
+
+        [HttpPost]
+        public ActionResult<List<Buku>> TambahBuku(Buku buku)
+        {
+            var books = ReadJson();
+            books.Add(buku);
+            WriteJson(books);
+
+            return ReadJson();
+        }
+
+        [HttpPut("{id}")]
+
+
+        [HttpDelete("{id}")]
+        public ActionResult<List<Buku>> HapusBuku(int id)
+        {
+            if ()
+            var books = ReadJson();
+            books.RemoveAt(id);
+            return ReadJson();
+        }
+    }
+}
---
+++ Models/Buku.cs
@@ -0,0 +1,23 @@
+﻿namespace tesKPL.Models
+{
+    public class Buku
+    {
+        public string judul;
+        public string penulis;
+        public string tahunTerbit;
+        public int tersedia;
+
+        public Buku()
+        {
+            
+        }
+
+        public Buku(string judul, string penulis, string tahunTerbit, int tersedia)
+        {
+            this.judul = judul;
+            this.penulis = penulis;
+            this.tahunTerbit = tahunTerbit;
+            this.tersedia = tersedia;
+        }
+    }
+}
