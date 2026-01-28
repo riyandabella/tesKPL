d226340bf61f9ca084221c75a308e118129d330d
riyandabella <riyandabella561@gmail.com>
2026-01-28 14:53:23 +0800

    singleton

---
+++ singleton.cs
@@ -0,0 +1,18 @@
+﻿namespace tesKPL
+{
+    public class singleton
+    {
+        private singleton() { }
+
+        private static singleton _instance;
+
+        public static singleton GetInstance() { 
+
+        if (_instance == null)
+            {
+            _instance = new singleton();
+    }
+               return _instance;
+ }
+    }
+}
